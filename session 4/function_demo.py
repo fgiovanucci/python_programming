11d19909f6e8a34fa9b11bcedb4cee08bf1a5384
fgiovanucci <fgiovanucci1@babson.edu>
2016-09-13 14:50:15 -0400

    Session 4

---
+++ session 4/function_demo.py
@@ -0,0 +1,49 @@
+
+def print_lyrics():
+    print("Hey Jude. Don't make it bad.")
+    print("Take a sad song and make it better.")
+
+#print(type(print_lyrics))
+#print_lyrics()
+
+def repeat_lyrics():
+    print_lyrics()
+    print('Na - na - na - na - na - na')
+    print_lyrics()
+
+repeat_lyrics()
+
+def print_twice(a):
+    print(a)
+    print(a)
+
+#print_twice('Babson')
+
+#my_name = 'Jerry'
+
+#print_twice(my_name)
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+
+line1 = 'Bing tiddle'
+line2 = 'tiddle bang'
+cat_twice(line1, line2)
+
+
+#print(cat_twice)
+'''
+def a_new_function_demo():
+    s = 0
+    for x in 'Chris':
+        print(x)
+        print(ord(x))
+        s = s + ord(x)
+    return s
+print(a_new_function_demo)
+'''
+
+
+input()
+
---
+++ session 4/my_abs.py
@@ -0,0 +1,21 @@
+def my_abs(n) :
+    if isinstance(n, int) or isinstance(n, float):          
+        if n >= 0:
+            return (n)
+        else: 
+            return (-n)
+    else: print ('invalid value')
+
+
+print(my_abs(100))
+
+print(my_abs(0))
+
+print(my_abs(-5))
+
+print(my_abs('a'))
+
+
+
+
+
