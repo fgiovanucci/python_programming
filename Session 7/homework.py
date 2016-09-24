4f30ff12e2c9f1ff718e8f9e19cc64575bb90036
fgiovanucci <fgiovanucci1@babson.edu>
2016-09-23 22:58:47 -0400

    Session 7

---
+++ Session 7/homework.py
@@ -0,0 +1,25 @@
+import math
+a = 4
+x = 3
+y = (x + a/x) / 2
+print(y)
+
+x = y
+y = (x + a/x) / 2
+print(y)
+
+from math import sqrt
+
+while True:
+    print(x)
+    y = (x + a/x) / 2
+    if y == x:
+        break
+def mysqrt(a):
+    x = a/2
+    while True:
+        y = (x + a/x) / 2
+        if y == x:
+            break
+        x = y
+    return x
