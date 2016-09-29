81723773cc982d39f27cb6f4c010b5f3db467469
fgiovanucci <fgiovanucci1@babson.edu>
2016-09-28 23:14:33 -0400

    Quiz1_2

--- Session 8/Quiz1.py
+++ Session 8/Quiz1.py
@@ -1,3 +1,4 @@
+from math import sqrt
 
 def crazy_about_9(a, b):
     if a == 9:
@@ -16,17 +17,38 @@ print(crazy_about_9(4, 5))
 print(crazy_about_9(3, 8))
 
 #2
-def leap_year(year):
-    if n%4==0 and n%100!=0:
-       elif n%400==0:
-            print(n,' is a leap year.'))
-    elif n%4!=0:
-        print(n, " is not a leap year.")
-
+def leap_year(i):
+    if i % 400 == 0:
+        return True
+    elif i % 100 == 0:
+        return False
+    elif i % 4 == 0:
+        return True
+    else:
+        return False
 print(leap_year(1900))
 print(leap_year(2016))
 print(leap_year(2017))
 print(leap_year(2000))
+
+
+#3
+def sum_squares(i):
+    sum = 0
+    for n in range(1,i+1):
+        if sqrt(n) / int(sqrt(n)) == 1:
+            sum += n
+            print(n)
+    return sum
+
+def sumsquares(i):
+    sum = 0
+    for n in range(1, int(sqrt(i))+1):
+        sum += n**2
+    return sum
+
+print(sum_squares(1))
+print(sum_squares(100))
 
    
     
